539b7c35c0ea1c03052c2e0693a497ecd7248edc
Shibanisankar Nayak <125339196+Sibani64578@users.noreply.github.com>
2024-12-16 16:37:29 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,14 @@
+const account_id=144553
+let account_Email="nayakshibani@gmail.com"
+var account_password="12345"
+account_city="Odisha"
+
+//account_id=2 // this is not allowed
+
+account_Email="hc@hc.com"
+account_password="121212"
+account_city="Bhadrak"
+console.log(account_id);
+
+console.table([account_id,account_Email,account_password,account_city]);
+
