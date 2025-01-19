7b22302daa362acf4615df924094b045dc54ced7
Shibanisankar Nayak <125339196+Sibani64578@users.noreply.github.com>
2025-01-20 00:10:28 +0530

    Add files via upload

---
+++ day6_conversionOperation.js
@@ -0,0 +1,27 @@
+let score =null;
+// let score ='33acd';
+// let score =undefind;
+
+console.log(typeof score);
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// '33'=>33
+//33ac=>NaN
+//true=>1;false=0
+
+// let isloggedIn=1
+// let booleanIsLoggedIn=Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//""=>false
+//"shibani"=>true
+
+let some_number=33;
+let string_num=String(some_number)
+console.log(string_num);
+console.log(typeof(string_num));
+
