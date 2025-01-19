643544f6219914355bd6a49915657edc52a17a48
Shibanisankar Nayak <125339196+Sibani64578@users.noreply.github.com>
2025-01-19 22:53:25 +0530

    Add files via upload

---
+++ day5_datatypes.js
@@ -0,0 +1,28 @@
+// "use strict"; //treat all Js code as newer version
+
+// alert("Hello"); //we are using node js not brower
+console.log("hello");
+console.log(3+3);//code readability showed be high
+
+// for (let i = 10; i <= 50; i += 2) {
+//     console.log(i);
+// }
+
+let name="Shibani";
+let age=23;
+let isloggedIn=false;
+//primitive datatype
+//number =>2 to power 53
+//bigint =>
+//String=>""
+//boolean=>true / false
+//null =>this is a standalown valu(repragentive of empty vallue)
+//undefind=>value not able to assine
+
+//symbol=>unique
+
+//non primitive datatype
+//object
+console.log(typeof("shibani"));
+console.log(typeof(undefined));
+console.log(typeof(null)); //object
