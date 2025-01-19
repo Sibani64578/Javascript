ea2004cad0530ed527e4ec25f6ffe1bc4f4baf18
Shibanisankar Nayak <125339196+Sibani64578@users.noreply.github.com>
2025-01-19 21:24:24 +0530

    Add files via upload

---
+++ day4_variables.js
@@ -0,0 +1,22 @@
+console.log("hello shibani");
+
+const Account_Id=144553;
+let Account_email="nayakshibnai@gamil.com";
+var account_password="1234";
+account_city="Bhadrak";
+ let account_state;
+console.log(Account_Id);
+
+//Account_Id=2;
+console.log(Account_Id);
+
+Account_email="nathgasy@gc.com"
+account_password="0977"
+account_city="odisaha"
+
+/**
+ * Prefer not to use var
+ * because of isuue of block scope and function scope
+ */
+console.table([Account_Id,Account_email,account_password,account_city,account_state]);
+
