bed4edd7e4f6aec951046a5725588200af89b97c
Shibanisankar Nayak <125339196+Sibani64578@users.noreply.github.com>
2025-01-22 10:41:41 +0530

    Add files via upload

---
+++ day11-string.js
@@ -0,0 +1,60 @@
+// const name="shibani"
+// const repoCount=50
+
+// //console.log(name + repoCount+ "nayak");
+// //console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// const game_name=new String("nayakshibani")
+
+// //console.log(game_name);
+// /*
+// console.log(game_name[0]);
+// console.log(game_name.__proto__);
+
+// console.log(game_name.length);
+// console.log(game_name.toUpperCase());
+// console.log(game_name.charAt(2));
+// console.log(game_name.indexOf('y'));
+
+// const new_string=game_name.substring(0,4);
+// console.log(new_string);
+
+// const another_string=game_name.slice(0,4);
+// console.log(another_string);
+// */
+// const new_stringone="      nayak     "
+// console.log(new_stringone);
+// console.log(new_stringone.trim());
+
+// const url ="https://hitesh.com/hitesh%20choudhayr";
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('hitesh'));
+// console.log(game_name.split('_'));
+
+
+const name="shibani"
+const repocount=50
+//console.log(name+repocount+"nayak");
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const game_name=new String("Shibani");
+console.log(game_name);
+
+console.log(game_name[0]);
+console.log(game_name.__proto__);
+
+console.log(game_name.length);
+console.log(game_name.toUpperCase());
+console.log(game_name.charAt(2));
+console.log(game_name.indexOf('i'));
+
+const new_string=game_name.substring(0, 4);
+console.log(new_string);
+
+const another_string=game_name.slice(0,4);
+console.log(another_string);
+
+const newstring="  nayak      "
+console.log(newstring);
+console.log(newstring.trim());
+
---
+++ day12-num_math.js
@@ -0,0 +1,46 @@
+// const score = 400
+// const balance =new Number(100)
+// //console.log(balance);
+// //console.log(score);
+
+// //console.log(balance.toString());
+// //console.log(balance.toFixed(2));
+
+// const other_number=1123.89666
+
+// //console.log(other_number.toPrecision(4));
+
+// const hundreds=1000000
+// //console.log(hundreds.toLocaleString('en-IN'));
+
+// //*********maths */
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));//top value
+// console.log(Math.floor(4.9));//button
+
+
+const score=100;
+console.log(score);
+
+const balance =new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const other_number=23.8966
+console.log(other_number.toPrecision(3));
+
+
+const hundreds=100000
+console.log(hundreds.toLocaleString('en-IN'));
+
+//------------maths---------//
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
