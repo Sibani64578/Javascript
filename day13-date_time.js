30c7782bafdc4f0dbbfc6f91300c5e55e414522c
Shibanisankar Nayak <125339196+Sibani64578@users.noreply.github.com>
2025-01-22 11:09:08 +0530

    Add files via upload

---
+++ day13-date_time.js
@@ -0,0 +1,28 @@
+// //date
+// /*
+// let my_date=new Date()
+// console.log(my_date.toString());
+// console.log(my_date.toDateString());
+
+// console.log(my_date.toLocaleString());
+// console.log(typeof my_date);
+// */
+// //let my_created_date=new Date(2023,0,23)
+// //console.log(my_created_date.toDateString());
+
+// let my_created_date=new Date("2023-01-14")
+// //console.log(my_created_date.toLocaleDateString());
+// let time_stamp=Date.now()
+// console.log(time_stamp);
+
+let my_date = new Date()
+console.log(my_date.toString());
+console.log(my_date.toDateString());
+console.log(my_date.toLocaleString());
+console.log(typeof my_date);
+
+let my_createdate=new Date(2023,0,23);
+console.log(my_createdate.toDateString());
+
+let time_stamp=Date.now()
+console.log(time_stamp);
